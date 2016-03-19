d0a9677f5be32a8a2d644fb5333f199d1030acd9
firdausshinoda <firdausns44@gmail.com>
2016-03-19 10:58:11 +0700

    version 1.0

---
+++ src/pemprogamankomputerdua/GenericArrayList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemprogamankomputerdua;
+
+/**
+ *
+ * @author Firdaus_Shinoda
+ */
+class GenericArrayList<T> {
+    
+}
